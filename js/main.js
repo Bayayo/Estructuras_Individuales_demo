fdb1204113a5dd2f8c6a3149c0ce7e58c4e9c920
diazsandra <diaz.zan@gmail.com>
2019-01-18 16:35:15 -0600

    se agregó buscador admin

--- js/main.js
+++ js/main.js
@@ -1462,3 +1462,19 @@ $('.mutliSelect input[type="checkbox"]').on('click', function() {
     }
 
 });
+
+//SEARCH ACCORDION
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
